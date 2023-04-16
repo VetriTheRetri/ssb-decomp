4c81378708ceb0597086d7ad1b6ebecedcb10763
VetriTheRetri <jupiter9764@gmail.com>
2023-04-16 15:33:15 +0300

    match fighter_status_squat.c

--- game/src/ft/common/fighter_status_pass.c
+++ game/src/ft/common/fighter_status_pass.c
@@ -32,3 +32,63 @@ void func_ovl3_80141E38(GObj *fighter_gobj)
 {
     func_ovl3_80141DA0(fighter_gobj, ftStatus_Common_GuardPass, 1.0F, 0U);
 }
+
+bool32 func_ovl3_80141E60(Fighter_Struct *fp)
+{
+    if ((fp->input.stick_range.y <= FTCOMMON_PASS_STICK_RANGE_MIN) && (fp->buffer_stick_y < FTCOMMON_PASS_BUFFER_FRAMES_MAX) && (fp->coll_data.clip_flag & 0x4000))
+    {
+        return TRUE;
+    }
+    else return FALSE;
+}
+
+bool32 func_ovl3_80141EA4(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+
+    if (func_ovl2_800F3794(fighter_gobj) != FALSE)
+    {
+        return func_ovl3_80147E7C(fighter_gobj);
+    }
+    else if (func_ovl3_80141E60(fp) != FALSE)
+    {
+        func_ovl3_80141E18(fighter_gobj);
+
+        return TRUE;
+    }
+    else return FALSE;
+}
+
+bool32 func_ovl3_80141F0C(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+
+    if (func_ovl2_800F3794(fighter_gobj) != FALSE)
+    {
+        return func_ovl3_80147E7C(fighter_gobj);
+    }
+    if (fp->status_vars.common.squat.is_allow_pass == FALSE)
+    {
+        if (func_ovl3_80141E60(fp) != FALSE)
+        {
+            fp->status_vars.common.squat.is_allow_pass = TRUE;
+            fp->status_vars.common.squat.pass_wait = FTCOMMON_SQUAT_PASS_WAIT;
+
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+bool32 func_ovl3_80141F8C(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+
+    if ((func_ovl3_80141E60(fp) != FALSE) && (fp->input.button_hold & fp->input.button_mask_z))
+    {
+        func_ovl3_80141E38(fighter_gobj);
+
+        return TRUE;
+    }
+    else return FALSE;
+}
--- game/src/ft/common/fighter_status_squat.c
+++ game/src/ft/common/fighter_status_squat.c
@@ -1,59 +1,200 @@
 #include "fighter.h"
 
-bool32 func_ovl3_80141E60(Fighter_Struct *fp)
-{
-    if ((fp->input.stick_range.y <= FTCOMMON_PASS_STICK_RANGE_MIN) && (fp->buffer_stick_y < FTCOMMON_PASS_BUFFER_FRAMES_MAX) && (fp->coll_data.clip_flag & 0x4000))
-    {
-        return TRUE;
-    }
-    else return FALSE;
-}
+#define ftStatus_CheckInterruptSquat(fighter_gobj) \
+(                                                  \
+    (func_ovl3_80151098(fighter_gobj) != FALSE) || \
+    (func_ovl3_80151160(fighter_gobj) != FALSE) || \
+    (func_ovl3_801511E0(fighter_gobj) != FALSE) || \
+    (func_ovl3_80149CE0(fighter_gobj) != FALSE) || \
+    (func_ovl3_80150470(fighter_gobj) != FALSE) || \
+    (func_ovl3_8015070C(fighter_gobj) != FALSE) || \
+    (func_ovl3_80150838(fighter_gobj) != FALSE) || \
+    (func_ovl3_8014F8C0(fighter_gobj) != FALSE) || \
+    (func_ovl3_8014FB1C(fighter_gobj) != FALSE) || \
+    (func_ovl3_8014FD70(fighter_gobj) != FALSE) || \
+    (func_ovl3_8014EC78(fighter_gobj) != FALSE) || \
+    (func_ovl3_80148D0C(fighter_gobj) != FALSE) || \
+    (func_ovl3_8014E764(fighter_gobj) != FALSE) || \
+    (func_ovl3_8013F4D0(fighter_gobj) != FALSE) || \
+    (func_ovl3_80141F0C(fighter_gobj) != FALSE) || \
+    (func_ovl3_80142258(fighter_gobj) != FALSE)    \
+)                                                  \
 
-bool32 func_ovl3_80141EA4(GObj *fighter_gobj)
+#define ftStatus_CheckInterruptSquatWait(fighter_gobj) \
+(                                                      \
+    (func_ovl3_80151098(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_80151160(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_801511E0(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_80149CE0(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_80150470(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_8015070C(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_80150884(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_8014F8C0(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_8014FB1C(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_8014FD70(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_8014EC78(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_80148D0C(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_8014E764(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_8013F4D0(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_8013ED64(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_80141EA4(fighter_gobj) != FALSE) ||     \
+    (func_ovl3_80142258(fighter_gobj) != FALSE)        \
+)                                                      \
+
+bool32 func_ovl3_80142E70(GObj *fighter_gobj)
 {
     Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
 
-    if (func_ovl2_800F3794(fighter_gobj) != FALSE)
+    if (fp->status_vars.common.squat.is_allow_pass != FALSE)
     {
-        return func_ovl3_80147E7C(fighter_gobj);
-    }
-    else if (func_ovl3_80141E60(fp) != FALSE)
-    {
-        func_ovl3_80141E18(fighter_gobj);
-
-        return TRUE;
-    }
-    else return FALSE;
-}
-
-bool32 func_ovl3_80141F0C(GObj *fighter_gobj)
-{
-    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
-
-    if (func_ovl2_800F3794(fighter_gobj) != FALSE)
-    {
-        return func_ovl3_80147E7C(fighter_gobj);
-    }
-    if (fp->status_vars.common.squat.is_allow_pass == FALSE)
-    {
-        if (func_ovl3_80141E60(fp) != FALSE)
+        if (fp->status_vars.common.squat.pass_wait != 0)
         {
-            fp->status_vars.common.squat.is_allow_pass = TRUE;
-            fp->status_vars.common.squat.pass_wait = FTCOMMON_SQUAT_PASS_WAIT;
+            fp->status_vars.common.squat.pass_wait--;
 
-            return TRUE;
+            if (fp->status_vars.common.squat.pass_wait == 0)
+            {
+                func_ovl3_80141DA0(fighter_gobj, ftStatus_Common_Pass, 1.0F, 0);
+
+                return TRUE;
+            }
         }
     }
+    if (fp->status_vars.common.squat.unk_0x8 != 0)
+    {
+        fp->status_vars.common.squat.unk_0x8--;
+    }
+
     return FALSE;
 }
 
-bool32 func_ovl3_80141F8C(GObj *fighter_gobj)
+void func_ovl3_80142ED8(GObj *fighter_gobj)
+{
+    func_ovl2_800D9480(fighter_gobj, func_ovl3_8014329C);
+}
+
+void func_ovl3_80142EFC(GObj *fighter_gobj)
+{
+    if (!ftStatus_CheckInterruptSquat(fighter_gobj))
+    {
+        func_ovl3_80142E70(fighter_gobj);
+    }
+}
+
+void func_ovl3_80143024(GObj *fighter_gobj)
 {
     Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
 
-    if ((func_ovl3_80141E60(fp) != FALSE) && (fp->input.button_hold & fp->input.button_mask_z))
+    func_ovl2_800E6F24(fighter_gobj, ftStatus_Common_Squat, 0.0F, 1.0F, 0U);
+    func_ovl2_800E0830(fighter_gobj);
+
+    fp->x192_flag_b0 = TRUE;
+
+    fp->status_vars.common.squat.is_allow_pass = FALSE;
+    fp->status_vars.common.squat.pass_wait = 0;
+
+    if ((fp->input.stick_range.y <= FTCOMMON_SQUAT_STICK_RANGE_MIN) && (fp->buffer_stick_y < FTCOMMON_SQUAT_BUFFER_FRAMES_MAX))
     {
-        func_ovl3_80141E38(fighter_gobj);
+        fp->status_vars.common.squat.unk_0x8 = 3;
+    }
+    else fp->status_vars.common.squat.unk_0x8 = 0;
+}
+
+void func_ovl3_801430A8(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+
+    func_ovl2_800E6F24(fighter_gobj, ftStatus_Common_Squat, 0.0F, 1.0F, 0U);
+    func_ovl2_800E0830(fighter_gobj);
+
+    fp->x192_flag_b0 = TRUE;
+
+    fp->status_vars.common.squat.is_allow_pass = TRUE;
+    fp->status_vars.common.squat.pass_wait = FTCOMMON_SQUAT_PASS_WAIT;
+    fp->status_vars.common.squat.unk_0x8 = 3;
+}
+
+bool32 func_ovl3_8014310C(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+
+    if (fp->input.stick_range.y <= (FTCOMMON_SQUAT_STICK_RANGE_MIN - 2))
+    {
+        func_ovl3_80143024(fighter_gobj);
+
+        return TRUE;
+    }
+    else return FALSE;
+}
+
+void func_ovl3_8014314C(GObj *fighter_gobj)
+{
+    return;
+}
+
+void func_ovl3_8014329C(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+
+    func_ovl2_800E6F24(fighter_gobj, ftStatus_Common_SquatWait, 0.0F, 1.0F, 0U);
+
+    fp->x192_flag_b0 = TRUE;
+
+    func_ovl2_800E7F7C(fighter_gobj, 120);
+}
+
+void func_ovl3_801432F0(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+
+    func_ovl2_800E6F24(fighter_gobj, ftStatus_Common_SquatWait, 0.0F, 1.0F, 0U);
+
+    fp->x192_flag_b0 = TRUE;
+
+    func_ovl2_800E7F7C(fighter_gobj, 120);
+
+    fp->status_vars.common.squat.is_allow_pass = FALSE;
+    fp->status_vars.common.squat.unk_0x8 = 0;
+    fp->status_vars.common.squat.pass_wait = 0;
+}
+
+bool32 func_ovl3_8014310C(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+
+    if (fp->input.stick_range.y <= (FTCOMMON_SQUAT_STICK_RANGE_MIN - 2))
+    {
+        func_ovl3_801432F0(fighter_gobj);
+
+        return TRUE;
+    }
+    else return FALSE;
+}
+
+void func_ovl3_80143394(GObj *fighter_gobj)
+{
+    if (!ftStatus_CheckInterruptSquatWait(fighter_gobj))
+    {
+        func_ovl3_8013E648(fighter_gobj);
+    }
+}
+
+void func_ovl3_801434CC(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+
+    func_ovl2_800E6F24(fighter_gobj, ftStatus_Common_SquatRv, 0.0F, 1.0F, 0U);
+    func_ovl2_800E0830(fighter_gobj);
+
+    fp->x192_flag_b0 = TRUE;
+}
+
+bool32 func_ovl3_8014351C(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+
+    if (fp->input.stick_range.y >= (FTCOMMON_SQUAT_STICK_RANGE_MIN + 4))
+    {
+        func_ovl3_801434CC(fighter_gobj);
 
         return TRUE;
     }
