194af592fe099ef8db9e1c38acd5b9081b4a8ca2
VetriTheRetri <jupiter9764@gmail.com>
2023-04-18 22:40:43 +0300

    some more cliff matches, finally matched action state change (on decomp.me at the moment)

--- game/src/ft/common/fighter_status_cliff.c
+++ game/src/ft/common/fighter_status_cliff.c
@@ -1,124 +1,47 @@
 #include "fighter.h"
 
-void func_ovl3_80144B30(GObj *fighter_gobj)
-{
-    func_ovl2_800D9480(fighter_gobj, func_ovl3_80144DF4);
-}
-
-void func_ovl3_80144B54(GObj *fighter_gobj)
-{
-    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
-    DObj *topn_joint = fp->joint[0], *transn_joint = fp->joint[1];
-    Vec3f vel;
-
-    if (fp->lr == RIGHT)
-    {
-        func_ovl2_800F4428(fp->coll_data.cliff_id, &vel);
-    }
-    else func_ovl2_800F4408(fp->coll_data.cliff_id, &vel);
-
-    topn_joint->translate.x = ((transn_joint->translate.z * fp->lr * topn_joint->scale.x) + vel.x);
-    topn_joint->translate.y = ((transn_joint->translate.y * topn_joint->scale.y) + vel.y);
-}
-
-void func_ovl3_80144C1C(GObj *fighter_gobj)
-{
-    return;
-}
-
-void func_ovl3_80144C24(GObj *fighter_gobj)
-{
-    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
-    Vec3f vel;
-
-    ftCollision_SetGround(fp);
-    ftStatus_Update(fighter_gobj, ftStatus_Common_CliffCatch, 0.0F, 1.0F, 0U);
-    ftAnim_Update(fighter_gobj);
-    func_ovl2_800DEEC8(fp);
-    func_ovl2_800D9444(fighter_gobj);
-
-    fp->coll_data.ground_line_id = -1;
-
-    func_ovl3_80144B54(fighter_gobj);
-
-    fp->x190_flag_b7 = TRUE;
-
-    if (fp->lr == RIGHT)
-    {
-        func_ovl2_800F4428(fp->coll_data.cliff_id, &vel);
-    }
-    else func_ovl2_800F4408(fp->coll_data.cliff_id, &vel);
-
-    func_ovl2_801016E0(&vel);
-
-    fp->cb_take_damage = &func_ovl3_80144CF8;
-
-    func_ovl2_800E8098(fp, 4U);
-}
-
-void func_ovl3_80144CF8(GObj *fighter_gobj)
-{
-    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
-    ObjectColl *object_coll = &fp->coll_data.object_coll;
-    Vec3f vel;
-
-    if (fp->lr == RIGHT)
-    {
-        func_ovl2_800F4428(fp->coll_data.cliff_id, &vel);
-    }
-    else func_ovl2_800F4408(fp->coll_data.cliff_id, &vel);
-
-    vel.x -= ((object_coll->width + 30.0F) * fp->lr);
-    vel.y -= object_coll->center;
-
-    func_ovl2_800DF014(fighter_gobj, &vel, &fp->coll_data);
-}
-
-void func_ovl3_80144DA4(GObj *fighter_gobj)
-{
-    if ((func_ovl3_80145620(fighter_gobj) == FALSE) && (func_ovl3_801457E0(fighter_gobj) == FALSE) && (func_ovl3_80145084(fighter_gobj) == FALSE))
-    {
-        func_ovl3_80144E84(fighter_gobj);
-    }
-}
-
-void func_ovl3_80144DF4(GObj *fighter_gobj)
+void func_ovl3_80144EE0(GObj *fighter_gobj)
 {
     Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
 
-    ftStatus_Update(fighter_gobj, ftStatus_Common_CliffWait, 0.0F, 1.0F, 0U);
-
-    fp->status_vars.common.cliff.status_id = 0;
-
-    if (fp->percent_damage < FTCOMMON_CLIFF_DAMAGE_HIGH)
+    if (fighter_gobj->anim_frame <= 0.0F)
     {
-        fp->status_vars.common.cliff.fall_wait = FTCOMMON_CLIFF_FALL_WAIT_DAMAGE_LOW;
+        switch (fp->status_vars.common.cliff.status_id)
+        {
+        case 0:
+            func_ovl3_801451F0(fighter_gobj);
+            break;
+
+        case 1:
+            func_ovl3_801456B4(fighter_gobj);
+            break;
+
+        case 2:
+            func_ovl3_8014586C(fighter_gobj);
+            break;
+        }
     }
-    else fp->status_vars.common.cliff.fall_wait = FTCOMMON_CLIFF_FALL_WAIT_DAMAGE_HIGH;
-
-    fp->x190_flag_b7 = TRUE;
-
-    func_ovl2_800E7F7C(fighter_gobj, 120);
-
-    fp->cb_take_damage = func_ovl3_80144CF8;
-
-    func_ovl2_800E8098(fp, 4U);
 }
 
-bool32 func_ovl3_80144E84(GObj *fighter_gobj)
+void func_ovl3_80144F64(GObj *fighter_gobj)
 {
     Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
 
-    fp->status_vars.common.cliff.fall_wait--;
-
-    if (fp->status_vars.common.cliff.fall_wait == 0)
+    if (fighter_gobj->anim_frame <= 0.0F)
     {
-        fp->cliffcatch_wait = FTCOMMON_CLIFF_CATCH_WAIT;
+        switch (fp->status_vars.common.cliff.status_id)
+        {
+        case 3:
+            func_ovl3_80145240(fighter_gobj);
+            break;
 
-        func_ovl3_80144CF8(fighter_gobj);
-        func_ovl3_801436A0(fighter_gobj);
+        case 4:
+            func_ovl3_80145704(fighter_gobj);
+            break;
 
-        return TRUE;
+        case 5:
+            func_ovl3_801458BC(fighter_gobj);
+            break;
+        }
     }
-    else return FALSE;
 }
---
+++ game/src/ft/common/fighter_status_cliff_catch_wait.c
@@ -0,0 +1,124 @@
+#include "fighter.h"
+
+void func_ovl3_80144B30(GObj *fighter_gobj)
+{
+    func_ovl2_800D9480(fighter_gobj, func_ovl3_80144DF4);
+}
+
+void func_ovl3_80144B54(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+    DObj *topn_joint = fp->joint[0], *transn_joint = fp->joint[1];
+    Vec3f vel;
+
+    if (fp->lr == RIGHT)
+    {
+        func_ovl2_800F4428(fp->coll_data.cliff_id, &vel);
+    }
+    else func_ovl2_800F4408(fp->coll_data.cliff_id, &vel);
+
+    topn_joint->translate.x = ((transn_joint->translate.z * fp->lr * topn_joint->scale.x) + vel.x);
+    topn_joint->translate.y = ((transn_joint->translate.y * topn_joint->scale.y) + vel.y);
+}
+
+void func_ovl3_80144C1C(GObj *fighter_gobj)
+{
+    return;
+}
+
+void func_ovl3_80144C24(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+    Vec3f vel;
+
+    ftCollision_SetGround(fp);
+    ftStatus_Update(fighter_gobj, ftStatus_Common_CliffCatch, 0.0F, 1.0F, 0U);
+    ftAnim_Update(fighter_gobj);
+    func_ovl2_800DEEC8(fp);
+    func_ovl2_800D9444(fighter_gobj);
+
+    fp->coll_data.ground_line_id = -1;
+
+    func_ovl3_80144B54(fighter_gobj);
+
+    fp->x190_flag_b7 = TRUE;
+
+    if (fp->lr == RIGHT)
+    {
+        func_ovl2_800F4428(fp->coll_data.cliff_id, &vel);
+    }
+    else func_ovl2_800F4408(fp->coll_data.cliff_id, &vel);
+
+    func_ovl2_801016E0(&vel);
+
+    fp->cb_take_damage = &func_ovl3_80144CF8;
+
+    func_ovl2_800E8098(fp, 4U);
+}
+
+void func_ovl3_80144CF8(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+    ObjectColl *object_coll = &fp->coll_data.object_coll;
+    Vec3f vel;
+
+    if (fp->lr == RIGHT)
+    {
+        func_ovl2_800F4428(fp->coll_data.cliff_id, &vel);
+    }
+    else func_ovl2_800F4408(fp->coll_data.cliff_id, &vel);
+
+    vel.x -= ((object_coll->width + 30.0F) * fp->lr);
+    vel.y -= object_coll->center;
+
+    func_ovl2_800DF014(fighter_gobj, &vel, &fp->coll_data);
+}
+
+void func_ovl3_80144DA4(GObj *fighter_gobj)
+{
+    if ((func_ovl3_80145620(fighter_gobj) == FALSE) && (func_ovl3_801457E0(fighter_gobj) == FALSE) && (func_ovl3_80145084(fighter_gobj) == FALSE))
+    {
+        func_ovl3_80144E84(fighter_gobj);
+    }
+}
+
+void func_ovl3_80144DF4(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+
+    ftStatus_Update(fighter_gobj, ftStatus_Common_CliffWait, 0.0F, 1.0F, 0U);
+
+    fp->status_vars.common.cliff.status_id = 0;
+
+    if (fp->percent_damage < FTCOMMON_CLIFF_DAMAGE_HIGH)
+    {
+        fp->status_vars.common.cliff.fall_wait = FTCOMMON_CLIFF_FALL_WAIT_DAMAGE_LOW;
+    }
+    else fp->status_vars.common.cliff.fall_wait = FTCOMMON_CLIFF_FALL_WAIT_DAMAGE_HIGH;
+
+    fp->x190_flag_b7 = TRUE;
+
+    func_ovl2_800E7F7C(fighter_gobj, 120);
+
+    fp->cb_take_damage = func_ovl3_80144CF8;
+
+    func_ovl2_800E8098(fp, 4U);
+}
+
+bool32 func_ovl3_80144E84(GObj *fighter_gobj)
+{
+    Fighter_Struct *fp = FighterGetStruct(fighter_gobj);
+
+    fp->status_vars.common.cliff.fall_wait--;
+
+    if (fp->status_vars.common.cliff.fall_wait == 0)
+    {
+        fp->cliffcatch_wait = FTCOMMON_CLIFF_CATCH_WAIT;
+
+        func_ovl3_80144CF8(fighter_gobj);
+        func_ovl3_801436A0(fighter_gobj);
+
+        return TRUE;
+    }
+    else return FALSE;
+}
